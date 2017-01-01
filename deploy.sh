3499ba3d595218d3f194ff42d6a558e3659b98af
ToutyRater <ToutyRater@users.noreply.github.com>
2017-01-01 18:20:24 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,13 @@ npm install gitbook-cli -g
 gitbook init
 gitbook install
 gitbook build
- 
--mkdir -p pages
--git clone https://github.com/ToutyRater/test.github.io.git pages
- 
-#rm -rf pages/*
-cp -rf _book/* pages/
 
-cd pages
+cd _book
 git init
 git config user.name "Rater"
 git config user.email "ToutyRater@users.noreply.github.com"
 git remote add upstream "https://${GH_TOKEN}@github.com/ToutyRater/test.github.io.git"
+
 git checkout -b gh-pages
 git add -A
 git commit -m 'update'
