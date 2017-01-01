1bb41dc5d69a7d8e70e8a2ffa001e1f0d810f173
ToutyRater <ToutyRater@users.noreply.github.com>
2017-01-01 17:32:37 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+npm install gitbook-cli -g
+gitbook init
+gitbook install
+gitbook build
+
+
+mkdir -p pages
+git clone https://github.com/ToutyRater/test.github.io.git pages
+
+rm -rf pages/*
+cp _book/* pages
+
+cd pages
+git init
+git config user.name "Rater"
+git config user.email "ToutyRater@users.noreply.github.com"
+git remote add upstream "https://${GH_TOKEN}@github.com/ToutyRater/test.github.io.git"
+git add -A
+git commit -m 'update'
+git push upstream master
