037d6ab372b4f9879511a997ec51fe6642e10bd3
ToutyRater <ToutyRater@users.noreply.github.com>
2017-01-01 17:51:42 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ git init
 git config user.name "Rater"
 git config user.email "ToutyRater@users.noreply.github.com"
 git remote add upstream "https://${GH_TOKEN}@github.com/ToutyRater/test.github.io.git"
+git checkout -b gh-pages
 git add -A
 git commit -m 'update'
 git push upstream gh-pages -f
